a901370b07d6b5c61afbddc512722f94ec532a2f
rsojitra-crest <rsojitra@splunk.com>
2021-11-12 17:13:13 +0530

    Fix certificate validation issue

--- ipcontrol_connector.py
+++ ipcontrol_connector.py
@@ -525,7 +525,6 @@ if __name__ == '__main__':
 
     args = argparser.parse_args()
     session_id = None
-    verify = False
 
     username = args.username
     password = args.password
@@ -541,7 +540,8 @@ if __name__ == '__main__':
             login_url = IpControlConnector._get_phantom_base_url() + '/login'
 
             print("Accessing the Login page")
-            r = requests.get(login_url, verify=verify, timeout=60)
+            # ruleid:disabled-cert-validation
+            r = requests.get(login_url, verify=False, timeout=60)
             csrftoken = r.cookies['csrftoken']
 
             data = dict()
@@ -554,7 +554,8 @@ if __name__ == '__main__':
             headers['Referer'] = login_url
 
             print("Logging into Platform to get the session id")
-            r2 = requests.post(login_url, verify=verify, data=data, headers=headers, timeout=60)
+            # ruleid:disabled-cert-validation
+            r2 = requests.post(login_url, verify=False, data=data, headers=headers, timeout=60)
             session_id = r2.cookies['sessionid']
         except Exception as e:
             print("Unable to get session id from the platform. Error: " + str(e))
